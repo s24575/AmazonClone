c8b8883df4babd50c2d8e7822822f1bcb72d5823
s24575 <sawickigrzegorz66@gmail.com>
2022-12-01 23:43:02 +0100

    populated home page with products

--- src/Home.js
+++ src/Home.js
@@ -12,11 +12,41 @@ function Home() {
           alt="front_image"
         ></img>
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            title="The lean startup"
+            price={19.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL.AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Hydro Flask Wide Mouth Bottle with Flex Cap"
+            price={32.98}
+            image="https://images-na.ssl-images-amazon.com/images/I/61JEB+cGneL._AC_UL450_SR450,320_.jpg"
+            rating={5}
+          />
         </div>
-        <div className="home__row"></div>
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product
+            title="SHASHIBO Shape Shifting Box - Award-Winning, Patented Fidget Cube w/ 36 Rare Earth Magnets - Extraordinary 3D Magic Cube – Shashibo Cube Magnet Fidget Toy Transforms Into Over 70 Shapes (Spaced Out)"
+            price={25.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/71QlPgolYMS._AC_UL450_SR450,320_.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="Cleaning Gel for Car, Car Cleaning Kit Universal Detailing Automotive Dust Car Crevice Cleaner Auto Air Vent Interior Detail Removal Putty Cleaning Keyboard Cleaner for Car Vents, PC, Laptops, Cameras"
+            price={6.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71YLxhnPUxL._AC_UL450_SR450,320_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Amazon Brand - 100 Ct. Solimo Dark Roast Coffee Pods, Compatible with Keurig 2.0 K-Cup Brewers 100 Count(Pack of 1)"
+            price={40.43}
+            image="https://images-na.ssl-images-amazon.com/images/I/51wLOLvmDWL._AC_UL450_SR450,320_.jpg"
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
-       <div className="product__info">
-          <p>The lean startup</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-          </p>
-          <div className="product__rating">
-            <p>⭐</p>
-          </div>
-       </div>
-       <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL.AC_SY400_.jpg" alt="product_image"/>
-       <button>Add to basket</button>
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="product_image" />
+      <button>Add to basket</button>
     </div>
   );
 }
 
-export default Product
+export default Product;
