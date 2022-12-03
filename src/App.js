994e6598046e53851998f38c490de6520fb60a68
s24575 <sawickigrzegorz66@gmail.com>
2022-12-03 05:29:31 +0100

    added checkout page WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home'
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={[<Header />, <Home />]} />
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          {/* <Route path = "/login" element={<Login />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/PrimeRewards/LP_Revamp/PC_Header_Banner._CB468631809_.jpg"
+          alt="advertisement"
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping basket</h2>
+          {/* BasketItem */}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
